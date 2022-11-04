04767a8bec0fbe9e8735024a2e91e56f8b7e7272
LAYSA LACERDA DE AQUINO <laysa.laquino@senacsp.edu.br>
2022-11-04 19:35:28 -0300

    ajax

--- ajax/ajax.js
+++ ajax/ajax.js
@@ -1,13 +1,16 @@
-document.querySelector('button').addEventListener('click',recuperaConteudoAjax);
+document.querySelector('button').addEventListener('click', recuperaConteudoAjax);
 
 function recuperaConteudoAjax(){
+
     const XHR = new XMLHttpRequest;
 
-    XHR.open('GET', 'http://dontpad.com;tsi-js-2202', true);
+    XHR.open('GET', 'http://127.0.0.1:5500/ajax/conteudo.txt', true);
 
     XHR.onload = function(){
+
         if(this.status == 200){
-            document.querySelector('#dadoRecuperado').innerHTML = this.responseText;
+            document.querySelector('#dadoRecuperado').innerHTML = this.responseText; 
         }
     }
-}
+    XHR.send();
+}        
